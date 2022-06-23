5d5139e58584a2eb97f852e0d4ff03944baa73e4
arifkenan <arif_kenan@hotmail.com>
2022-06-23 23:52:58 +0300

    Add project files.

---
+++ DotNetCore.DataTransfer.ToView/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using DotNetCore.DataTransfer.ToView.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DotNetCore.DataTransfer.ToView.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public List<Product> GetProductList
+        {
+            get
+            
+            {
+                List<Product> products = new List<Product>();
+                products.Add(new Product { Id = 1, ProductName = "Elma", Price = 20, Stock = 10 });
+                products.Add(new Product { Id = 2, ProductName = "Armut", Price = 22, Stock = 10 });
+                products.Add(new Product { Id = 3, ProductName = "Karpuz", Price = 23, Stock = 10 });
+                products.Add(new Product { Id = 4, ProductName = "Kavun", Price = 24, Stock = 10 });
+
+                return products;
+            }
+        }
+
+        public IActionResult List()
+        {
+            TempData["ProductList"] = JsonConvert.SerializeObject(GetProductList);
+            ViewBag.ProductList = JsonConvert.SerializeObject(GetProductList);
+            ViewData["ProductList"] = JsonConvert.SerializeObject(GetProductList);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Index()
+        {
+            var tempDataVeri = TempData["ProductList"];
+            var viewBagDataVeri = ViewBag.ProductList;
+            var viewDataVeri = ViewData["ProductList"];
+
+            ViewBag.UrunListesi = JsonConvert.DeserializeObject<List<Product>>(tempDataVeri.ToString());
+
+            return View();
+        }
+    }
+}
